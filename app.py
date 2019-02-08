197cbd4e860197a9fe7db296cfb8ff5184719743
Zain <zalind@gmail.com>
2019-02-07 19:52:08 -0800

    ZA/YR Setup basic SQL database

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
+from datetime import datetime
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
- 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(120), unique=True, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    category = db.Column(db.Text, nullable=True)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
