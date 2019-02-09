238ed5a4c4dfe9427715b74f15a69047cbad8673
Daniel Gamliel <dgamliel@umail.ucsb.edu>
2019-02-08 16:16:03 -0800

    adding upload functionality to our webapp

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, \
+                  request, url_for
+
 app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = {'jpg', 'png', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+
+    #check if the post request has the file
+    if 'file' not in request.files:
+        flash('no file part')
+        return redirect(request.url)
+
+
+    file = request.files['file']
+
+    if file.filename == '':
+        flash('
+
  
 @app.route('/')
 def index():
