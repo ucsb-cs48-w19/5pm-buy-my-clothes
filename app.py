2fd2fb48a55e87511ff215f19242a32ab8971ed9
Zain <zalind@gmail.com>
2019-02-12 16:01:03 -0800

    Debugging

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-#from models import db, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
