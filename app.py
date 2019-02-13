03c9b3cb7a5cf99c521cdf1f07b82484bd6afbd4
Daniel Gamliel <dgamliel@umail.ucsb.edu>
2019-02-12 18:33:00 -0800

    adding db and some functionality

--- app.py
+++ app.py
@@ -4,27 +4,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-#Uncomment for deployment
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
-#Comment for local testing
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/test')
-def test_route():
-    posts = get_posts()
-    return render_template('test.html', posts=posts)
-
-if __name__ == "__main__":
-    app.run()
 
 def get_posts():
     return Post.query.all()
@@ -37,4 +17,26 @@ class Post(db.Model):
     pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-return '<Post %r>' % self.id
+        return '<Post %r>' % self.id
+
+#Uncomment for deployment
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+#Comment for local testing
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.getcwd() , 'database/app.db')
+print(app.config['SQLALCHEMY_DATABASE_URI'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/test')
+def test_route():
+    posts = get_posts()
+    
+    return render_template('test.html', posts=posts)
+
+if __name__ == "__main__":
+    app.run()
+
