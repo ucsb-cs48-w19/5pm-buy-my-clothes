10faae22a5ad0bbcec92a9bda86debe679446a82
Yinon Rousso <yinon@169-231-122-101.wireless.ucsb.edu>
2019-02-26 16:19:59 -0800

    added test

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ import pytest
 from app import parse_filename
    
 def test_parse_filename1():
-   assert parse_filename("image.png") == "image"
+   assert parse_filename("image.png") == ('image', 'png')
    
