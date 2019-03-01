790464081fe7d0f4ab6c2c2fd228e6f97ba4908c
Michelle Su <michellesu@umail.ucsb.edu>
2019-02-28 17:46:50 -0800

    Added tests for parsing filenames

--- test_app.py
+++ test_app.py
@@ -3,5 +3,16 @@ import pytest
 from app import parse_filename
    
 def test_parse_filename1():
-   assert parse_filename("image.png") == ('image', 'png')
-   
+	assert parse_filename("image.png") == ('image', 'png')
+
+def test_parse_filename2():
+	assert parse_filename("image.jpg") == ('image', 'jpg')
+
+def test_parse_filename3():
+	assert parse_filename("image.gif") == ('image', 'gif')
+
+def test_parse_filename4():
+	assert parse_filename("image.jpeg") == ('image', 'jpeg')
+
+def test_parse_filename5():
+	assert parse_filename("image.exe") == ('None', 'None')
